1862493a15d15214e1a7a0cfa2e8eabe4d06f15f
user <revarad95@gmail.com>
2022-10-26 20:31:15 +0300

    git commit

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ require('dotenv').config()
 
 const express = require('express')
 const sequelize = require('./db')
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 5050
 const models = require('./models/models')
 const app = express()
 const cors = require('cors')
@@ -17,7 +17,6 @@ app.use('/api', router)
 
 //
 app.use(errorHandler)
-
 const start = async () => {
     try {
         await sequelize.authenticate()
